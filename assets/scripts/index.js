a2b19fbbfd62d2f0dd8019de57628b0bc7db3a89
Cris Mouta <crismouta@gmail.com>
2021-04-26 14:31:38 +0200

    reset button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,10 @@
 import { participantsList } from './participants.js'
 
+for (let item = 0; item < participantsList.length; item++) {
+    let getUl = document.querySelector(".listName")
+    getUl.innerHTML += `<li>${participantsList[item]}</li>`
+}
+
 const cannonButton = document.querySelector(".cannon")
 
 function functionPack() {
@@ -19,6 +24,12 @@ function getRandomName(lista) {
     geth2.innerHTML = ""
     geth2.innerHTML = `${lista[randomValue]}`
     lista.splice(randomValue, 1)
+    for (let item = 0; item < lista.length; item++) {
+        let getUl = document.querySelector(".listName")
+        getUl.innerHTML = ""
+        getUl.innerHTML += `<li>${lista[item]}</li>`
+        
+    }
 }
 
 function consolelog() {
@@ -27,10 +38,15 @@ function consolelog() {
 
 cannonButton.onclick = functionPack
 
-for (let index = 0; index < participantsList.length; index++) {
-    let getUl = document.querySelector(".listName")
-    getUl.innerHTML += `<li>${participantsList[index]}</li>`
-}
 
-let getResetButton = document.getElementById()
+let getResetButton = document.getElementById('reset')
+getResetButton.onclick = functionReset
+
+function functionReset() {
+    participantsList = ["Cris"]
+    participantsList = ["David S", "Diego", "Cris", "Snezhanna", "Abdulha", "David H", 
+    "Alexandra", "Andrés", "Andrea C", "Andrea S", "Gabriela", "Gabi", "Amr", "Anya", "Armando", 
+    "Berta", "Franco", "Jael", "Joan", "Jose Miguel", "Leah", "Martín", "Solomon", "Sergio", 
+    "Thierno"]
+}
 
