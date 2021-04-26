7ed65215bafbdceae037603e832e5fe399c33e55
David <david.hdm9@gmail.com>
2021-04-26 14:28:05 +0200

    remove participants from the list

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,10 +1,26 @@
 import { participantsList } from './participants.js'
 
+/*suma()
+function suma() {
+    let letras = 'abc'
+    let numeros = 4 + 10
+    return letras + ' Hola me llamo David ' + numeros
+}
+console.log(suma())
+
+document.querySelector(".header-title").innerHTML = suma()*/
+
+let getUl = document.querySelector(".listName")
+for (let index = 0; index < participantsList.length; index++) {
+    getUl.innerHTML += `<li>${participantsList[index]}</li>`
+}
+
 const cannonButton = document.querySelector(".cannon")
 
 function functionPack() {
     getRandomName(participantsList)
     fireBoat()
+    deleteName(participantsList)
     consolelog()
 }
 
@@ -15,10 +31,19 @@ function fireBoat() {
 
 function getRandomName(lista) {
     let randomValue = Math.floor(Math.random() * lista.length)
+    return randomValue
+}
+
+function deleteName(lista) {
     const geth2 = document.querySelector(".result")
+    const randomValue = getRandomName(lista)
     geth2.innerHTML = ""
     geth2.innerHTML = `${lista[randomValue]}`
     lista.splice(randomValue, 1)
+    getUl.innerHTML = ""
+    for (let i = 0; i < lista.length; i++){
+        getUl.innerHTML += `<li>${lista[i]}</li>`
+    }
 }
 
 function consolelog() {
@@ -27,10 +52,5 @@ function consolelog() {
 
 cannonButton.onclick = functionPack
 
-for (let index = 0; index < participantsList.length; index++) {
-    let getUl = document.querySelector(".listName")
-    getUl.innerHTML += `<li>${participantsList[index]}</li>`
-}
-
-let getResetButton = document.getElementById()
+//let getResetButton = document.getElementById()
 
