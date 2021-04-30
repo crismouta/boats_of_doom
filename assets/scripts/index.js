cb12ccf73aedae0e01ed5f2b66d8b191d3b741b8
David <david.hdm9@gmail.com>
2021-04-30 02:03:34 +0200

    asd

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,58 +1,122 @@
-import { genesisDeadList, genesisList } from './participants.js';
-let participantsList = genesisList();
-let deadList = genesisDeadList();
+import { genesisDeadList, genesisList } from './participants.js'
 
-const cannonButton = document.querySelector(".cannon");
-const geth2 = document.querySelector(".result");
+let participantsList = genesisList()
+let deadList = genesisDeadList()
 
+const cannonButton = document.querySelector(".cannon")
+const geth2 = document.querySelector(".result")
+const getAliveUL = document.querySelector(".listName")
+const getDeadUL = document.querySelector(".deadList")
+const getBoat = document.querySelector(".boat2")
+const getTextInput = document.querySelector(".textInput")
+const getResetButton = document.getElementById('reset')
 
-cannonButton.onclick = functionPack;
+cannonButton.onclick = runFunctions
+getResetButton.onclick = resetLists
 
-function functionPack() {
-    cannonSound();
-    getRandomName();
-    setTimeout(printName, 1500);
-    setTimeout(removeFromAliveList, 1500);
+for (let i = 0; i < participantsList.length; i++) {
+    getAliveUL.innerHTML += `<li>${participantsList[i]}</li>`
+}
+
+function runFunctions() {
+    cannonSound()
+    setTimeout(getRandomValue, 1500)
+    setTimeout(fireBoat, 1500)
+    setTimeout(resetBoat, 8500)
 }
 
 // Sonido del cañón
 function cannonSound() {
-    let audio = document.getElementById("audio");
+    let audio = document.getElementById("audio")
+
     if (audio.paused) {
-        audio.play();
+        audio.play()
     }  
 }
 
 // Elegir un valor aleatorio del array al dar click al cañón
-function getRandomName() {
-    let randomName = participantsList[Math.floor(Math.random() * participantsList.length)];
-    return randomName;
+const getRandomValue = () => {
+    let randomName = Math.floor(Math.random() * participantsList.length)
+
+    printName(randomName)
+    removeFromAliveList(randomName)
 }
 
 // Imprimir el nombre en el H2 al darle al boton del cañón
-function printName() {
-    const useRandomName = getRandomName();
-    geth2.innerHTML = useRandomName;
-    removeFromAliveList(useRandomName);
-    /* setTimeout(() => {
-        geth2.innerHTML = `${lista[randomValue]}`
-        lista.splice(randomValue, 1)
-        getUl.innerHTML = ""
-        for (let i = 0; i < lista.length; i++){
-        getUl.innerHTML += `<li>${lista[i]}</li>`
-    }}, 1500); */
+function printName(randomName) {
+    let chosenParticipant = participantsList[randomName]
+    geth2.innerHTML = chosenParticipant
+    console.log(randomName)
+
+    addToDeadList(chosenParticipant)
 }
 
 // Eliminar el nombre de la lista de vivos y del array
-function removeFromAliveList(useRandomName) {
-    participantsList.splice(useRandomName, 1);
-    console.log(participantsList);
+function removeFromAliveList(randomName) {
+    participantsList.splice(randomName, 1)
+    console.log(participantsList)
+    getAliveUL.innerHTML = ""
+
+    for (let i = 0; i < participantsList.length; i++) {
+        getAliveUL.innerHTML += `<li>${participantsList[i]}</li>`
+    }
 }
 
 // Añadir el nombre a la lista de muertos y al array
+function addToDeadList(chosenParticipant) {
+    deadList.unshift(chosenParticipant)
+    console.log(deadList)
+    getDeadUL.innerHTML = ""
+
+    for (let i = 0; i < deadList.length; i++) {
+        getDeadUL.innerHTML += `<li>${deadList[i]}</li>`
+    }
+}
 
 // Hacer que el barco arda durante un tiempo cuando el nombre sea elegido
+function fireBoat() {
+    getBoat.src = "./assets/img/fireboat.png"
+}
+
+function resetBoat() {
+    getBoat.src = "./assets/img/boat.png"
+    geth2.innerHTML = ""
+}
 
 // Añadir a la lista de vivos el nombre introducido en el input
+getTextInput.addEventListener('keydown', function getInputValue(event) {
+    let key = event.keyCode || event.which
+
+    if (key === 13){
+        let inputValue = getTextInput.value
+        addNameToList(inputValue)
+    }
+})
+function addNameToList(inputValue) {
+    participantsList.unshift(inputValue)
+    console.log(participantsList)
+    getAliveUL.innerHTML = ""
+
+    for (let i = 0; i < participantsList.length; i++){
+            getAliveUL.innerHTML += `<li>${participantsList[i]}</li>`
+    }
+
+    getTextInput.value = ""
+}
 
 // Resetear la lista de participantes al darle al boton de Reset, y vaciar la lista de muertos
+function resetLists() {
+    participantsList = genesisList()
+    deadList = genesisDeadList()
+
+    getAliveUL.innerHTML = ""
+    getDeadUL.innerHTML = ""
+    geth2.innerHTML = ""
+
+    for (let i = 0; i < participantsList.length; i++){
+        getAliveUL.innerHTML += `<li>${participantsList[i]}</li>`
+    }
+
+    console.log(participantsList, deadList)
+    resetBoat()
+}
